7a4419f4ffa0ff2f60922a24b99645bc25f8159b
Israel Nogueira <israel@feats.com.br>
2026-02-03 23:10:07 -0300

    test: add basic test structure

---
+++ Tests/Unit/ExampleTest.php
@@ -0,0 +1,9 @@
+<?php
+namespace IsraelNogueira\PaymentHub\Tests\Unit;
+use PHPUnit\Framework\TestCase;
+class ExampleTest extends TestCase{
+    public function test_example(): void
+    {
+    	$this->assertTrue(true);
+    }
+}
